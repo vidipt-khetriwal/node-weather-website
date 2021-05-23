9a5633fdd18f1f113b04aec8d0120be12fe48f9b
Vidipt Khetriwal <mohangoyal@Vidipts-MacBook-Pro.local>
2021-05-23 09:59:54 +0530

    Descriptive Weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (longitude,latitude,callback) => {
         } else if (body.error) {
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0]+". It is "+body.current.temperature+" degrees out. It feels like " + body.current.feelslike + " degrees")
+            callback(undefined,body.current.weather_descriptions[0]+". It is "+body.current.temperature+" degrees out. It feels like " + body.current.feelslike + " degrees. Humidity is " + body.current.humidity+"%.")
         }
 })
 }
